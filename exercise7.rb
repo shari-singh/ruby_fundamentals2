3d4d609dc846125fa84c982f5c3d32627098de4c
shari-singh <shari.singh93@gmail.com>
2016-06-01 16:29:28 -0400

    added cohort and increase students by 5%

--- exercise7.rb
+++ exercise7.rb
@@ -3,11 +3,17 @@ students = {
   cohort2: 42,
   cohort3: 22,
 }
-
 def list(students)
   students.each do |cohort, amount|
   puts "#{cohort}: #{amount} students"
 end
 end
-
 list(students)
+puts "--------"
+students [:cohort4] = 43
+puts students.keys
+puts "--------"
+
+students.each do |cohorts, new_amount|
+  puts "#{cohorts}: #{new_amount + (new_amount * 0.05).to_i} students"
+end
