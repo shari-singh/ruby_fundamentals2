ad08d255c75a63d3c2b3e95f55214e04f06d0dd5
shari-singh <shari.singh93@gmail.com>
2016-06-01 15:26:11 -0400

    fixed bug in method list

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,11 @@
 grocery_list = ["marshmallows", "graham crackers", "chocolate"]
 
 def list(grocery_list)
-  grocery_list.map do |food|
-    puts "* #{food}" 
+  grocery_list.each do |food|
+    puts "* #{food}"
   end
 end
 
 grocery_list << "rice"
 
-puts list(grocery_list)
+list(grocery_list)
