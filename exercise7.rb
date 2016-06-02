f62cf48ef0021ee508ccbd3f6e4927dab9b639d4
shari-singh <shari.singh93@gmail.com>
2016-06-01 22:54:54 -0400

    added total number of students

--- exercise7.rb
+++ exercise7.rb
@@ -3,20 +3,24 @@ students = {
   cohort2: 42,
   cohort3: 22,
 }
-def list(students)
+def list(students)  #method to output cohorts and class size
   students.each do |cohort, amount|
   puts "#{cohort}: #{amount} students"
 end
 end
 list(students)
-puts "--------"
-students [:cohort4] = 43
-puts students.keys
-puts "--------"
 
-students.each do |cohorts, new_amount|
-  puts "#{cohorts}: #{new_amount + (new_amount * 0.05).to_i} students"
+students [:cohort4] = 43  #add new cohort 4 with 43 students
+
+puts "---- Cohort Names ---------"
+puts students.keys  #output cohort names
+
+puts "--- New Cohort Amounts -----"
+students.each do |cohorts, new_amount|  #calculate class size increase 5%
+  puts "#{cohorts}: #{new_amount + (new_amount * 0.05).to_i} students" #.to_i to make the value a whole number
 end
-puts "--------"
+puts "---- Removed Cohort 2 ----"
 students.delete(:cohort2)
 list(students)
+
+puts "Total Students: #{students.values.reduce(0) {|sum, num| sum + num}}" #iterate using reduce to keep count of the total
