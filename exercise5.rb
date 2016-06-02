54f4ead480e031a519914feb4f9ed04103ef3c3c
shari-singh <shari.singh93@gmail.com>
2016-06-01 22:53:50 -0400

    added comments

--- exercise5.rb
+++ exercise5.rb
@@ -1,8 +1,8 @@
-puts "What is the temperaure in Fahrenheit?"
+puts "What is the temperature in Fahrenheit?"
 f = gets.to_i  #gets the temp and converts it to an integer
 
 def temp_f(f)
   (f-32) * (5/9)  #formula for converting F to C
 end
 
-puts "#{f} F = #{temp_f(f)} C" 
+puts "#{f} F = #{temp_f(f)} C"
